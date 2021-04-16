3e71648bae933e8efba741804a5679ea31458d31
Steve LeValley <slevalley@gmail.com>
2021-04-16 14:37:34 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// const fs = require('fs').promises;
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+
+const noteList = [];
+
+// Routes
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+// app.post('/reserve', (req, res) => {
+    
+//     const newNote = req.body;
+
+// });
+
+
+
+
+
+// Displays all notes
+
+// app.get('/api/notes', (req, res) => res.json(noteList));
+
+
+
+// Starts the server to begin listening
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
