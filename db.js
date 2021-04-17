e55520c8861dac4783d5bbed1c38ff7c3b756240
Steve LeValley <slevalley@gmail.com>
2021-04-17 16:56:25 -0700

    Lost

--- db.js
+++
@@ -1,9 +0,0 @@
-const noteArray = 
-  [
-    {
-      "title": "Yesterday's To Do's",
-      "text": "Take the trash out"
-    }
-  ];
-  
-module.exports = noteArray;
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const db = require('./db.js');
-// const noteArray = require('noteArray');
-
-console.log(db);
-
+const fs = require('fs').promises;
+const db = require('./db.json');
+const { nanoid } = require("nanoid");
 
 
 // Sets up the Express App
@@ -17,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./public")));
 
-  
+
 
 
 // console.log(noteList);
@@ -28,12 +25,86 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.htm
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 // app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.get("/api/notes", (req, res) => res.json(db));
 
 
-app.post("/api/notes", (req, res) => {
-    const {title, text} = req.body;
+
+app.get("/api/notes", (req, res) => {
+
+    const currentNotes = fs.readFileSync("./db.json", "utf-8")
+    .then((data) => JSON.parse(data)); 
+    console.log(data);
+    res.json(currentNotes);
 });
+ 
+    
+
+
+
+
+
+// app.post("/api/notes", (req, res) => 
+    
+//     fs.readFile((path.join(__dirname, "/db.js", 'utf-8', function(err, data){
+//         res.json(req.body)
+//     })
+    
+//     ));
+
+
+
+
+// const readNotes = () =>
+//     fs.readFile(path.join(__dirname, "/db.js"), "utf-8")
+//         .then((data) => console.log(data));
+
+// const makeNote = (title, noteText) => {
+//     if (!title || noteText) {
+//         return Promise.reject(new Error("Cannot have an empty title or text"));
+//     } 
+    
+    
+//     fs.readFile(path.join(__dirname, "/db.js"), "utf-8")
+//         .then((data) => JSON.parse(data))
+    
+//     .then((noteArray) => {
+//         const note = {
+//             // id: nanoid(),
+//             title,
+//             noteText,
+//         };
+//         noteArray.push(note);
+//         return fs.writeFile(path.join(__dirname, "/db.js"), JSON.stringify(movies), "utf-8")
+//             .then(() => note);
+//     });
+// };
+
+
+// app.post("api/notes", (req, res) => {
+//     res(req.body);
+//     // const { title, noteText } = req.body;
+//     // makeNote(title, noteText)
+//     // .then(() => res("hello"))
+//     // .catch((err) => {
+//     //     console.log(err);
+//     //     res.sendStatus(400);
+//     // });
+// });
+
+
+// app.post("/api/notes", (req, res) => {
+//     newNoteInfo = req.body;
+//     let filePath = path.join(__dirname, "/db.js");
+//     noteArray.push(newNoteInfo);
+
+//     fs.writeFile(filePath, JSON.stringify(noteArray), function (err) {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log("Note saved successfully!");
+//     })
+
+//     res.json(noteArray);
+// });
 
 
 
